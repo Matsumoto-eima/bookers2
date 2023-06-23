59ad29cc137c3667d066f441eb8a730ece264b2b
Matsumoto-eima <135197614+Matsumoto-eima@users.noreply.github.com>
2023-06-23 04:39:23 +0000

    Forth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_out_path_for(resource)
+    "/"
+  end
+ def after_sign_up_path_for(_resource)
+    index_book_path
+  end
   protected
 
   def configure_permitted_parameters
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
- resources :users, only: [:show, :edit, :index]
+  devise_for :users
+  resources :users, only: [:show, :edit, :index]
+
   resources :books, only: [:new, :index, :show, :create]
   root to: "homes#top"
   get "/homes/about" => "homes#about" ,as: "about"
-  devise_for :users
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
